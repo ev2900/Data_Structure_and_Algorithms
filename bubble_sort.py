418a89606ee1cbe5a9c79973f36591f67b52f996
Christopher Sharkey <ev2900@gmail.com>
2018-02-05 13:44:57 -0500

    updated bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -4,7 +4,7 @@
 # Run time of O(n^2) by many considerations a slow sorting algorithum   
 #
 
-# the outer loop controls the number of passes needed to sort everything. The number of passes need is the lenght of the list - 1
+# the outer loop controls the number of passes needed to sort everything. Each pass of the bubble sorts 1 element. The first pass ensures the highest number right most. The second pass ensure the second higest ... The number of passes need is the lenght of the list - 1.
 def bubbleSort(mylist):
     for i in range(0, len(mylist)-1):
         #innter loop is moving the buble along
@@ -13,5 +13,8 @@ def bubbleSort(mylist):
                 mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
     return mylist
 
-ex_list = [1, 12, 6, 8, 9, 14]
+ex_list = [1, 12, 6, 8, 9, 14, 12, 1]
 print(bubbleSort(ex_list))
+
+# for a list of 8 the outer loop will make 7 iterations
+# for a list of 8 the inner loop will make 7 iterations
